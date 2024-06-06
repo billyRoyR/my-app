a66db78f24fe0fe381779389397e525d23950179
billyRoyR <156974090+billyRoyR@users.noreply.github.com>
2024-06-06 08:21:36 +0100

    rr

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Heroimg from '../components/Heroimg';
 
 const Home = () => {
   return <div>
     <Navbar/>
+    <Heroimg />
   </div>;
 };
 
